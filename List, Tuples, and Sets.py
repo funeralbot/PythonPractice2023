156832b56d818a69d08950c29fa7eec86195b14b
funeralbot <spaceshifts@gmail.com>
2023-10-27 07:00:15 -0700

    Udemy Python Refresher Course 2023

---
+++ List, Tuples, and Sets.py
@@ -0,0 +1,17 @@
+#List, Sets, and Tumples are different collections that allow us to store multiple values in a single variable
+l = ["Bob", "Rolf", "Anne"] #list can have elements added and removed
+#list are the most common because you can add and remove elements
+
+t = ("Bob", "Rolf", "Anne") #tuples are set and cannot be changed
+
+s = {"Bob", "Rolf", "Anne"} #you can add and remove elements from a set but you cannot have duplicate elements in a set
+#the order of a set is not always guarenteed
+
+print(l[0]) #subscript notation }
+
+l.append("Smith") #add elements to a list with the append function
+
+l.remove("Bob") #remove elements from a list
+
+s.add("Smith") #add elements to a set
+
