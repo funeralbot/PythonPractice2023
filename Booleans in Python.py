6f284001df8627bd374627068132e20a8171224c
funeralbot <spaceshifts@gmail.com>
2023-11-06 07:06:29 -0800

    Udemy Python Refresher Course 2023

---
+++ Booleans in Python.py
@@ -0,0 +1,15 @@
+print( 5 == 5) #equal to
+
+print( 5 > 5) #less than
+
+print( 10 != 10) #not equal to
+
+# comparisons: equal to ==, not equal to != , greater than >, less than <, not greater than >=, not less than <=
+
+friends = ["Rolf", "Bob"]
+
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad) #compare two list in python to see if the elements insdie are the same
+
+print( friends is abroad) #compare two elements to see if they are the same 
