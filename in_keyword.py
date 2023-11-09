c06a13125ddf91eb335ef21bebf957f0ea65f17c
funeralbot <spaceshifts@gmail.com>
2023-11-09 07:14:35 -0800

    Udemy python refresher course 2023

---
+++ in_keyword.py
@@ -0,0 +1,8 @@
+friends = ["Rolf", "Bob", "Jen"] #list with names
+print ("Jen" in friends) #use the in keyword to see if element in list is true
+
+movies_watched = {"The Matrix", "Green Book", "Her"} #a set containing variables
+
+user_movie = input("Enter something you've watched recently: ") #input asking user if variable is in set
+
+print(user_movie in movies_watched) #display result of user input true or false 
